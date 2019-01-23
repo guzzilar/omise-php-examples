428c541e4971ff8dca2ec963cabab7b267dc1578
guzzilar <iam.nuttanon@gmail.com>
2019-01-23 13:33:36 +0700

    _nav.php, fixing hardcoded-links.

--- _nav.php
+++ _nav.php
@@ -1,20 +1,20 @@
 <ul class="section-nav">
     <li>
-        <a href="/omise">index</a>
+        <a href=".">index</a>
     </li>
 
-    <li><a href="/omise/account.php">Acccount</a></li>
-    <li><a href="/omise/balance.php">Balance</a></li>
-    <li><a href="/omise/card.php">Card</a></li>
-    <li><a href="/omise/charge.php">Charge</a></li>
-    <li><a href="/omise/customer.php">Customer</a></li>
-    <li><a href="/omise/dispute.php">Dispute</a></li>
-    <li><a href="/omise/event.php">Event</a></li>
-    <li><a href="/omise/forex.php">Forex</a></li>
-    <li><a href="/omise/link.php">Link</a></li>
-    <li><a href="/omise/recipient.php">Recipient</a></li>
-    <li><a href="/omise/schedule.php">Schedule & Occurrence</a></li>
-    <li><a href="/omise/source.php">Source</a></li>
-    <li><a href="/omise/transaction.php">Transaction</a></li>
-    <li><a href="/omise/transfer.php">Transfer</a></li>
+    <li><a href="./account.php">Acccount</a></li>
+    <li><a href="./balance.php">Balance</a></li>
+    <li><a href="./card.php">Card</a></li>
+    <li><a href="./charge.php">Charge</a></li>
+    <li><a href="./customer.php">Customer</a></li>
+    <li><a href="./dispute.php">Dispute</a></li>
+    <li><a href="./event.php">Event</a></li>
+    <li><a href="./forex.php">Forex</a></li>
+    <li><a href="./link.php">Link</a></li>
+    <li><a href="./recipient.php">Recipient</a></li>
+    <li><a href="./schedule.php">Schedule & Occurrence</a></li>
+    <li><a href="./source.php">Source</a></li>
+    <li><a href="./transaction.php">Transaction</a></li>
+    <li><a href="./transfer.php">Transfer</a></li>
 </ul>
