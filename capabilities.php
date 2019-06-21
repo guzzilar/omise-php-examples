a71b50ebfe51ee94a1b50a4c2309454a763fcabc
guzzilar <iam.nuttanon@gmail.com>
2019-06-21 15:07:57 +0800

    Adding Omise\Capabilities reload case.

--- capabilities.php
+++ capabilities.php
@@ -6,4 +6,13 @@
             <?php display_result($capabilities); ?>
         </div>
     </div>
+
+    <!-- Reload OmiseCapabilities -->
+    <div class="col col-6">
+        <div class="col-body content">
+            <h4>Reload capabilities</h4>
+            <?php $capabilities->reload(); ?>
+            <?php display_result($capabilities); ?>
+        </div>
+    </div>
 </div>
